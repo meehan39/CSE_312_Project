980873c7e9304280bc0cde8b7b68c387673f1765
ethanhal <60364246+ethanhal@users.noreply.github.com>
2021-04-15 10:52:00 -0400

    minor updates to html and css

--- server/app.py
+++ server/app.py
@@ -9,9 +9,9 @@ users = []
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-UPLOAD_FOLDER = r'C:\Users\jonat\Desktop\Atom Projects\Project\server\static\uploads'
+UPLOAD_FOLDER = r'static/uploads'
 
-app.config['UPLOAD_FOLDER'] = r'C:\Users\jonat\Desktop\Atom Projects\Project\server\static\uploads'
+app.config['UPLOAD_FOLDER'] = r'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 allowed_picture_types = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -70,10 +70,6 @@ def validate_password(password):
 
     return False
 
-@app.route("/settings")
-def settings():
-    return render_template('settings.html')
-
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
@@ -86,25 +82,33 @@ def chat():
 def friends():
     return render_template('friends.html')
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_picture_types
 
-
+"""
+Dosen't save username across this call. This is fine because we will probalby use 
+cookies to save username. Will probably have to do something similar for the picture itself
+because it dosent stay after navigating away from the feed page.
+"""
 
 #saves the uploaded file to the static folder
 @app.route('/home', methods=['POST'])
 def upload_image():
+    username = request.args.get('username')
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template('home.html', filename=filename)
+
+
+        return render_template('home.html', filename=filename, username=username)
+
+
 #displays the image on the page
 @app.route('/display/<filename>')
 def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
